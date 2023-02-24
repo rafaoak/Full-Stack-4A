a4afb9afa875150fc13bfd2b3fe7eeddd36246f0
Rafaela Carvalho <carvalho.sims@gmail.com>
2023-02-23 22:18:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function Enviar(){
+
+    var nome = document.getElementById("nomeid");
+    var tel = document.getElementById("foneid");
+    var email = document.getElementById("emailid");
+    (nome.value != ""); {
+        alert('Obrigada sr(a) ' + nome.value + 'Os seus dados, Tel: '+ tel.value +'E-mail:' + email.value +'. foram enviados com sucesso');
+}
+
+    }
+    
