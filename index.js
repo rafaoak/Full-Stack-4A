a30fc6f5a4271bb73afd427893c949572a4c6202
Rafaela Carvalho <carvalho.sims@gmail.com>
2023-02-23 22:21:48 -0300

    Aula1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Enviar(){
     var tel = document.getElementById("foneid");
     var email = document.getElementById("emailid");
     (nome.value != ""); {
-        alert('Obrigada sr(a) ' + nome.value + 'Os seus dados, Tel: '+ tel.value +'E-mail:' + email.value +'. foram enviados com sucesso');
+        alert('Obrigada sr(a) ' + nome.value + ' Os seus dados, Tel: '+ tel.value +'E-mail:' + email.value +'. foram enviados com sucesso');
 }
 
     }
